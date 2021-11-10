30e91c2f986b4fa22408dad59b56493e7a44f926
Sean Kim <seaki@microsoft.com>
2021-11-09 22:27:33 -0800

    improved copy authorization policy; ask user for purpose and identity type

--- src/commands/copyAuthorizationPolicy.ts
+++ src/commands/copyAuthorizationPolicy.ts
@@ -15,12 +15,52 @@ export async function copyAuthorizationPolicy(context: IActionContext, node?: Au
         node = authorizationNode;
     }
 
+    // Select purpose
+    var attachToken = "Attach the access token to the backend call";
+    var tokenBack = "Get the access token back";
+    var purposeOptions = [ attachToken, tokenBack ];
+    const purposeSelected = await ext.ui.showQuickPick(
+        purposeOptions.map(purpose => { return { label: purpose, description: '', detail: '' }; }), 
+        { placeHolder: 'How do you want to use the policy?', canPickMany: false });
+    
+    // Select identity 
+    var managed = "managed";
+    var jwt = "jwt";
+    var identityTypeOptions = [
+        {
+            label: managed,
+            description: "Use the managed identity of the APIM service."
+        },
+        {
+            label: jwt,
+            description: "Use the identity of the specified token."
+        }
+    ]
+    const identityTypeSelected = await ext.ui.showQuickPick(
+        identityTypeOptions.map(option => { return { label: option.label, description: option.description, detail: '' }; }), 
+        { placeHolder: 'Which identity type do you want to use?', canPickMany: false });
+
     const pid = node.root.authorizationProviderName;
     const aid = node.authorizationContract.name;
 
-    vscode.env.clipboard.writeText(`<get-authorization-context provider-id="${pid}" authorization-id="${aid}" context-variable-name="${pid}-${aid}-context" identity-type="managed" ignore-error="true" />
+    if (identityTypeSelected.label == managed) {
+        var identityPhrase = `identity-type=${identityTypeSelected.label}`;
+    } else {
+        var identityPhrase = `identity-type=${identityTypeSelected.label} identity={jwt with audience="https://management.core.windows.net/"}`
+    }
+
+    if (purposeSelected.label == attachToken) {
+        var policy = `<!-- Add to the inbound policy -->
+<get-authorization-context provider-id="${pid}" authorization-id="${aid}" context-variable-name="${pid}-${aid}-context" ignore-error="true" ${identityPhrase} />
 <set-header name="Authorization" exists-action="override">
     <value>Bearer @((Authorization)@(((Authorization)context.Variables.GetValueOrDefault("${pid}-${aid}-context"))?.AccessToken)</value>
-</set-header>`);
+</set-header>`
+    } else {
+        var policy = `<!-- Add to the outbound policy -->
+<get-authorization-context provider-id="${pid}" authorization-id="${aid}" context-variable-name="${pid}-${aid}-context" ignore-error="true" ${identityPhrase} />
+<set-body template="none">@(((Authorization)context.Variables.GetValueOrDefault("authContext"))?.AccessToken)</set-body>`;
+    }
+
+    vscode.env.clipboard.writeText(policy);
     vscode.window.showInformationMessage(localize("CopySnippet", `Policy copied to clipboard.`));
 }
