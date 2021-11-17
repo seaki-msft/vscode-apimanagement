0d7ac90309dcfa29ebfce9287f390d4155898c43
Sean Kim <seaki@microsoft.com>
2021-11-17 06:06:52 -0800

    CopyAuthorizationPolicy: get identity attribute from the incoming Authorization header

--- src/commands/copyAuthorizationPolicy.ts
+++ src/commands/copyAuthorizationPolicy.ts
@@ -16,7 +16,6 @@ export async function copyAuthorizationPolicy(context: IActionContext, node?: Au
     }
 
     // Select purpose
-    // TODO(seaki): check wording with Adrian
     var attachToken = "Attach the access token to the backend call";
     var tokenBack = "Get the access token back";
     var purposeOptions = [ attachToken, tokenBack ];
@@ -46,22 +45,35 @@ export async function copyAuthorizationPolicy(context: IActionContext, node?: Au
 
     if (identityTypeSelected.label == managed) {
         // TODO(seaki): error if managed identity is not turned on or permission has not been created
+        var comment = `<!-- Add to the inbound policy -->`;
         var identityPhrase = `identity-type="${identityTypeSelected.label}"`;
         var additionalMessage = "For 'managed' identity-type, make sure managed identity is turned on."
     } else {
-        var identityPhrase = `identity-type="${identityTypeSelected.label}" identity="{jwt with audience='https://management.core.windows.net/'}"`
-        var additionalMessage = "For 'jwt' identity-type, please provide a jwt with audience='https://management.core.windows.net/' to 'identity' attribute."
+        var allowedAudienceMessage = `Allowed audiences for jwt in "identity" attribute are "https://management.core.windows.net/" and "https://apihub.azure.com/"`;
+        var comment = `<!-- Add to the inbound policy. ${allowedAudienceMessage} -->`;
+        var identityPhrase = `identity-type="${identityTypeSelected.label}" identity="@(context.Request.Headers["Authorization"][0].Replace("Bearer ", ""))"`;
+        var additionalMessage = `For 'jwt' identity-type, ${allowedAudienceMessage}`;
     }
 
     if (purposeSelected.label == attachToken) {
-        var policy = `<!-- Add to the inbound policy -->
-<get-authorization-context provider-id="${pid}" authorization-id="${aid}" context-variable-name="${pid}-${aid}-context" ignore-error="false" ${identityPhrase} />
+        var policy = `${comment}
+<get-authorization-context 
+    provider-id="${pid}"
+    authorization-id="${aid}" 
+    context-variable-name="${pid}-${aid}-context" 
+    ignore-error="false" 
+    ${identityPhrase} />
 <set-header name="Authorization" exists-action="override">
     <value>@("Bearer " + ((Authorization)context.Variables.GetValueOrDefault("${pid}-${aid}-context"))?.AccessToken)</value>
 </set-header>`
     } else {
-        var policy = `<!-- Add to the inbound policy -->
-<get-authorization-context provider-id="${pid}" authorization-id="${aid}" context-variable-name="${pid}-${aid}-context" ignore-error="false" ${identityPhrase} />
+        var policy = `${comment}
+<get-authorization-context 
+    provider-id="${pid}" 
+    authorization-id="${aid}" 
+    context-variable-name="${pid}-${aid}-context" 
+    ignore-error="false" 
+    ${identityPhrase} />
 <return-response>
     <set-status code="200" />
     <set-body template="none">@(((Authorization)context.Variables.GetValueOrDefault("${pid}-${aid}-context"))?.AccessToken)</set-body>
@@ -70,4 +82,5 @@ export async function copyAuthorizationPolicy(context: IActionContext, node?: Au
 
     vscode.env.clipboard.writeText(policy);
     vscode.window.showInformationMessage(localize("CopySnippet", `Policy copied to clipboard. ${additionalMessage}`));
+    ext.outputChannel.appendLine(`Policy copied to clipboard. ${additionalMessage}`);
 }
